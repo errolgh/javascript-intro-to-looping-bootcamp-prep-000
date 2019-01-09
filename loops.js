bf84b59656d5e27299b0f8e5d53702e795a4b2cb
Errol W <ewatsonrei@gmail.com>
2019-01-09 04:33:34 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,10 +2,10 @@
 var array = [];
 
 function forLoop(array) {
-  for (let i = 0; i <= 25; i++) {
     if (i === 1) {
       return `I am 1 strange loop.`;
     } else {
+      for (let i = 0; i <= 25; i++) {
       return `I am ${i} strange loops.`;
     }
   }
