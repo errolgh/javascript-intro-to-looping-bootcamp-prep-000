14c5a534ccf88f0e377d7f02c9a225a5a4c57643
Errol W <ewatsonrei@gmail.com>
2019-01-09 03:54:35 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,14 +1,18 @@
 //for loop 
-function forLoop (array) {
-  for (let i = 0; i < 25; i++) {
+var array = [];
+
+function forLoop(array) {
+  for (let i = 0; i <= 25; i++) {
     if (i === 1) {
-      console.log(`I am ` + i + ` strange loop.`);
+      return `I am 1 strange loop.`;
     } else {
-      console.log(`I am ` + i + ` strange loops.`);
+      
+      return `I am ${i} strange loops.`;
     }
   }
-  return array;
 }
+
+
 
 //while loop
 function whileLoop (n) {
@@ -19,5 +23,7 @@ function whileLoop (n) {
     console.log(--countdown);
   }
 }
+
+
 // i think the above while loop is bad because it looks
 //like it will go on forever.
